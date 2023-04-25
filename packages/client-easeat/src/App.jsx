b4a4eb772ecb9b83bb927d293c019b42a1b050d4
Adam <adam.ouali@icloud.com>
2023-04-25 15:58:47 +0200

    Ajout des composants tailwind et quelques vues

--- packages/client-easeat/src/App.jsx
+++ packages/client-easeat/src/App.jsx
@@ -1,40 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import MainNavBar from './Components/MainNavBar/MainNavBar.jsx'
-import BodyTest from './Components/BodyTest/BodyTest.jsx'
-import Button from 'react-bootstrap/Button'
-import { Container, Row, Col } from 'react-bootstrap'
-import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar'
-import Footer from './Components/Footer/Footer.jsx'
+import './index.css'
+import Login from './Components/Login/Login'
+import Sidebar from './Components/Sidebar/Sidebar';
+
 
 function App() {
     return (
         <>
             <div className="App">
-                <MainNavBar />
-                <Container fluid className='fill-height'>
-                    <Row className='test fill-height'>
-                        <Col>
-                            <Sidebar>
-                                <Menu>
-                                    <MenuItem> Accueil</MenuItem>
-                                    <MenuItem> Calendar</MenuItem>
-                                    <MenuItem> Test </MenuItem>
-                                </Menu>
-                            </Sidebar>
-                        </Col>
-                        <Col className='bg-info'>
-                            <h1> BodyTest </h1>
-                        </Col>
-                    </Row>
-                </Container>
-                <Footer></Footer>
+                {/* <Login /> */}
+                <Sidebar />
             </div>
         </>
     )
 }
 
-export default App
+
+export default App;
---
+++ packages/client-easeat/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,197 @@
+import React from 'react'
+import easeatLogoCropped from '../../assets/easeat_logo_cropped.png'
+
+export default function Sidebar() {
+    return (
+        <div className="flex">
+            <div className="flex flex-col h-screen p-3 bg-gray-800 shadow w-60">
+                <div className="space-y-3">
+                    <div className="flex items-center mx-8">
+                        <img
+                            className="mx-auto h-13 w-auto"
+                            src={easeatLogoCropped}
+                            alt="Logo Easeat"
+                        />
+                    </div>
+                    <div className="relative"></div>
+                    <div className="flex-1">
+                        <ul className="pt-2 pb-4 space-y-1 text-sm">
+                            <li className="rounded-sm">
+                                <a
+                                    href="#"
+                                    className="flex items-center p-2 space-x-3 rounded-md"
+                                >
+                                    <span className="text-gray-100">
+                                        Accueil dashboard
+                                    </span>
+                                </a>
+                            </li>
+                            <li className="rounded-sm">
+                                <a
+                                    href="#"
+                                    className="flex items-center p-2 space-x-3 rounded-md"
+                                >
+                                    <span className="text-gray-100">
+                                        Profils
+                                    </span>
+                                </a>
+                            </li>
+                            <li className="rounded-sm">
+                                <a
+                                    href="#"
+                                    className="flex items-center p-2 space-x-3 rounded-md"
+                                >
+                                    <span className="text-gray-100">
+                                        Orders
+                                    </span>
+                                </a>
+                            </li>
+                            <li className="rounded-sm">
+                                <a
+                                    href="#"
+                                    className="flex items-center p-2 space-x-3 rounded-md"
+                                >
+                                    <span className="text-gray-100">
+                                        Settings
+                                    </span>
+                                </a>
+                            </li>
+                            <li className="rounded-sm">
+                                <a
+                                    href="#"
+                                    className="flex items-center p-2 space-x-3 rounded-md"
+                                >
+                                    <span className="text-gray-100">
+                                        Logout
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="container mx-auto mt-12 p-8">
+                <div className="grid grid-cols-1 gap-6 mb-6 ">
+                    <div className="w-full h-52 px-4 py-5 bg-white rounded-lg shadow overflow-auto">
+                        oui
+                    </div>
+                    <div className="w-full h-52 px-4 py-5 bg-white rounded-lg shadow overflow-auto">
+                        <table class="min-w-full">
+                            <thead class="bg-white border-b">
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                                    >
+                                        Horodatage
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                                    >
+                                        Type
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                                    >
+                                        Message
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="bg-gray-100 border-b">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                        <p>25/04/2023 - 12:01</p>
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <p>Alerte</p>
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <p>Température frigo : 5°C</p>
+                                    </td>
+                                </tr>
+                                <tr class="bg-gray-100 border-b">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                        <p>12:01</p>
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <p>Alerte</p>
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <p>Température frigo : 5°C</p>
+                                    </td>
+                                </tr>                                <tr class="bg-gray-100 border-b">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                        <p>12:01</p>
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <p>Alerte</p>
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <p>Température frigo : 5°C</p>
+                                    </td>
+                                </tr>                                <tr class="bg-gray-100 border-b">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                        <p>12:01</p>
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <p>Alerte</p>
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <p>Température frigo : 5°C</p>
+                                    </td>
+                                </tr>                                <tr class="bg-gray-100 border-b">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                        <p>12:01</p>
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <p>Alerte</p>
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <p>Température frigo : 5°C</p>
+                                    </td>
+                                </tr>                                <tr class="bg-gray-100 border-b">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                        <p>12:01</p>
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <p>Alerte</p>
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <p>Température frigo : 5°C</p>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    {/* <div className="w-full px-4 py-5 bg-white rounded-lg shadow">
+                        <div className="text-sm font-medium text-gray-500 truncate">
+                            Total users
+                        </div>
+                        <div className="mt-1 text-3xl font-semibold text-gray-900">
+                            12,00
+                        </div>
+                    </div>
+                    <div className="w-full px-4 py-5 bg-white rounded-lg shadow">
+                        <div className="text-sm font-medium text-gray-500 truncate">
+                            Total Profit
+                        </div>
+                        <div className="mt-1 text-3xl font-semibold text-gray-900">
+                            $ 450k
+                        </div>
+                    </div>
+                    <div className="w-full px-4 py-5 bg-white rounded-lg shadow">
+                        <div className="text-sm font-medium text-gray-500 truncate">
+                            Total Orders
+                        </div>
+                        <div className="mt-1 text-3xl font-semibold text-gray-900">
+                            20k
+                        </div>
+                    </div> */}
+                </div>
+            </div>
+        </div>
+    )
+}
