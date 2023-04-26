7d8c6040aa69d8b76853fdc3d80df7dc41207964
EliseVP <eliseverhoest@yahoo.com>
2023-04-26 12:16:24 +0200

    Début de la page ajout de profil

--- packages/client-easeat/src/App.jsx
+++ packages/client-easeat/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Form } from 'react-router-dom';
 
 import Login from './components/Login/Login.jsx'
 import Sidebar from './components/Sidebar/Sidebar.jsx'
 import Error404 from './pages/Error404';
+import AddProfile from './components/AddProfile/AddProfile.jsx'
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                         <Route path="/" element={<Sidebar />} />
                         <Route path="/login" element={<Login />} />
                         <Route path="*" element={<Error404 />} />
+                        <Route path="/TestProfile" element={<AddProfile />} />
                     </Routes>
                 </BrowserRouter>
 
