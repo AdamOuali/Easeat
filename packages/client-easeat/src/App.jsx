09d2e3eaa68e0cd4100c5cf4122f2e5890ccbb82
Adam <adam.ouali@icloud.com>
2023-04-26 11:50:59 +0200

    Utilisation de la sidebar OK via composant externe

--- packages/client-easeat/src/App.jsx
+++ packages/client-easeat/src/App.jsx
@@ -1,28 +1,26 @@
 import { useState } from 'react'
 import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './components/Login/Login.jsx'
 import Sidebar from './components/Sidebar/Sidebar.jsx'
-import Error404 from './pages/Error404';
+import Error404 from './pages/Error404'
+import Calendrier from './pages/Calendrier'
 
 function App() {
     return (
         <>
-            <div className="App">
-
+            <div class="grid grid-cols-4">
                 <BrowserRouter>
                     <Routes>
                         <Route path="/" element={<Sidebar />} />
                         <Route path="/login" element={<Login />} />
+                        <Route path="/calendrier" element={<Calendrier />} />
                         <Route path="*" element={<Error404 />} />
                     </Routes>
                 </BrowserRouter>
-
             </div>
         </>
     )
 }
 
-
-export default App;
+export default App
