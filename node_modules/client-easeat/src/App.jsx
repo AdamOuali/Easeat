0393f5989a4f0076e0372f45e2580f409b3e018a
EliseVP <eliseverhoest@yahoo.com>
2023-04-27 09:31:50 +0200

    Test new push Elise + modif css page AddProfile

--- node_modules/client-easeat/src/App.jsx
+++ node_modules/client-easeat/src/App.jsx
@@ -2,29 +2,30 @@ import { useState } from 'react'
 import './index.css'
 import { BrowserRouter, Routes, Route, Form } from 'react-router-dom';
 
+
+import AddProfile from './components/AddProfile/AddProfile.jsx'
 import Login from './components/Login/Login.jsx'
 import Sidebar from './components/Sidebar/Sidebar.jsx'
-import Error404 from './pages/Error404';
-import AddProfile from './components/AddProfile/AddProfile.jsx'
+import Error404 from './pages/Error404'
+import Calendrier from './pages/Calendrier'
+import DashboardHome from './pages/DashboardHome'
 
 function App() {
     return (
         <>
-            <div className="App">
-
+            <div className="grid grid-cols-4">
                 <BrowserRouter>
                     <Routes>
-                        <Route path="/" element={<Sidebar />} />
+                        <Route path="/" element={<DashboardHome />} />
                         <Route path="/login" element={<Login />} />
+                        <Route path="/calendrier" element={<Calendrier />} />
                         <Route path="*" element={<Error404 />} />
                         <Route path="/TestProfile" element={<AddProfile />} />
                     </Routes>
                 </BrowserRouter>
-
             </div>
         </>
     )
 }
 
-
-export default App;
+export default App
--- packages/client-easeat/src/components/AddProfile/AddProfile.jsx
+++ packages/client-easeat/src/components/AddProfile/AddProfile.jsx
@@ -8,6 +8,7 @@ import Vegetables from '../../assets/vegetables.png'
 import { NavLink } from 'react-router-dom'
 import '../../components/AddProfile/AddProfile.css';
 import '../../index.css'
+import SideBar from '../../components/Sidebar/Sidebar.jsx';
 
 function profileForm() {
   const [lastName, setLastName] = useState("");
@@ -32,52 +33,52 @@ function profileForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">Nom </label>
-        <input className="px-3 py-2 border border-gray-800 rounded-md focus:border-indigo-500 " type="text" id="lastName" value={lastName} onChange={(event) => setLastName(event.target.value)} />
-      </div>
-      <div>
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstName">Prénom </label>
-        <input className="px-3 py-2 border border-gray-800 rounded-md focus:border-indigo-500" type="text" id="firstname" value={firstName} onChange={(event) => setFirstName(event.target.value)} />
-      </div>
-      
-      {/* Temps pour cuisiner */}
-      <div>
-        <legend className="block text-gray-700 text-sm font-bold mb-2" >Avez-vous du temps pour cuisiner ?</legend>
-        <input type="checkbox" id="checkbox1" checked={isCheckedTime} onChange={(event) => setIsCheckedTime(event.target.checked)} />
-        <label htmlFor="checkbox1">
-          <img src={TimeToCook} alt="Time to cook" />
-        </label>
-	<input type="checkbox" id="checkbox2" checked={isCheckedRegular} onChange={(event) => setIsCheckedRegular(event.target.checked)} />
-	<label htmlFor="checkbox2">
-          <img src={RegularTimeToCook} alt="Regular time to cook" />
-        </label>
-	<input type="checkbox" id="checkbox3" checked={isCheckedNoTime} onChange={(event) => setIsCheckedNoTime(event.target.checked)} />
-	<label htmlFor="checkbox3">
-          <img src={NoTimeToCook} alt="No time to cook" />
-        </label>
-      </div>
-      
-{/* Type d'alimentation */}
-      <div>
-        <legend className="block text-gray-700 text-sm font-bold mb-2" >Type d'alimentation</legend>
-        <input type="checkbox" id="checkbox4" checked={isCheckedVege} onChange={(event) => setIsCheckedVege(event.target.checked)} />
-        <label htmlFor="checkbox4">
-          <img src={Vegetables} alt="Vegetarien" />
-        </label>
-	<input type="checkbox" id="checkbox5" checked={isCheckedMeat} onChange={(event) => setIsCheckedMeat(event.target.checked)} />
-	<label htmlFor="checkbox5">
-          <img src={Meat} alt="Omnivore" />
-        </label>
-	<input type="checkbox" id="checkbox6" checked={isCheckedGastro} onChange={(event) => setIsCheckedGastro(event.target.checked)} />
-	<label htmlFor="checkbox6">
-          <img src={gastronomy} alt="Gastronomie" />
-        </label>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">Nom </label>
+          <input className="px-3 py-2 border border-gray-800 rounded-md focus:border-indigo-500 " type="text" id="lastName" value={lastName} onChange={(event) => setLastName(event.target.value)} />
+        </div>
+        <div>
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstName">Prénom </label>
+          <input className="px-3 py-2 border border-gray-800 rounded-md focus:border-indigo-500" type="text" id="firstName" value={firstName} onChange={(event) => setFirstName(event.target.value)} />
+        </div>
+        
+        {/* Temps pour cuisiner */}
+        <div>
+          <legend className="block text-gray-700 text-sm font-bold mb-2" >Avez-vous du temps pour cuisiner ?</legend>
+          <input type="checkbox" id="checkbox1" checked={isCheckedTime} onChange={(event) => setIsCheckedTime(event.target.checked)} />
+          <label htmlFor="checkbox1">
+            <img src={TimeToCook} alt="Time to cook" />
+          </label>
+    <input type="checkbox" id="checkbox2" checked={isCheckedRegular} onChange={(event) => setIsCheckedRegular(event.target.checked)} />
+    <label htmlFor="checkbox2">
+            <img src={RegularTimeToCook} alt="Regular time to cook" />
+          </label>
+    <input type="checkbox" id="checkbox3" checked={isCheckedNoTime} onChange={(event) => setIsCheckedNoTime(event.target.checked)} />
+    <label htmlFor="checkbox3">
+            <img src={NoTimeToCook} alt="No time to cook" />
+          </label>
+        </div>
+        
+  {/* Type d'alimentation */}
+        <div>
+          <legend className="block text-gray-700 text-sm font-bold mb-2" >Type d'alimentation</legend>
+          <input type="checkbox" id="checkbox4" checked={isCheckedVege} onChange={(event) => setIsCheckedVege(event.target.checked)} />
+          <label htmlFor="checkbox4">
+            <img src={Vegetables} alt="Vegetarien" />
+          </label>
+    <input type="checkbox" id="checkbox5" checked={isCheckedMeat} onChange={(event) => setIsCheckedMeat(event.target.checked)} />
+    <label htmlFor="checkbox5">
+            <img src={Meat} alt="Omnivore" />
+          </label>
+    <input type="checkbox" id="checkbox6" checked={isCheckedGastro} onChange={(event) => setIsCheckedGastro(event.target.checked)} />
+    <label htmlFor="checkbox6">
+            <img src={gastronomy} alt="Gastronomie" />
+          </label>
+        </div>
 
-      <button type="submit">Envoyer</button>
-    </form>
+        <button className="mt-4" type="submit">Envoyer</button>
+      </form>
   );
 }
 
