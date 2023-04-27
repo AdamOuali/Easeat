5f3a0826126275974825f028657a6261c4e4fbc7
EliseVP <eliseverhoest@yahoo.com>
2023-04-27 11:56:45 +0200

    push elise

--- node_modules/client-easeat/tailwind.config.js
+++ node_modules/client-easeat/tailwind.config.js
@@ -2,11 +2,11 @@
 export default {
     content: ['./src/**/*.{js,jsx,ts,tsx}', './src/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
-        colors: {
-            primary: '#2D668E',
+        extend: {
+            colors: {
+                primary: '#2D668E',
+            },
         },
     },
-
     plugins: [],
 }
